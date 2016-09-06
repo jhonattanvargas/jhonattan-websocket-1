48fdc6ee08a3a4281916cf5d47610e2bd3b52fda
jhonattan vargas <jhonattan.vargas@gmail.com>
2016-09-06 17:21:44 -0300

    primer commit
    primer commit desc

---
+++ public/main.js
@@ -0,0 +1,25 @@
+var socket = io.connect('http://localhost:8080',{'forceNew':true});
+
+socket.on('messages',function(data){
+	console.log(data);
+	render(data);
+})
+
+function render(data){
+	var html = data.map(function(elem,index){
+		return(`<div>
+					<strong>${elem.author}</strong>:
+					<en>${elem.text}</en>
+				</div>`);
+	}).join(" ");
+	document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e){
+	var payload ={
+		author: document.getElementById('username').value,
+		text: document.getElementById('texto').value
+	};
+	socket.emit('new-message',payload);
+	return false;
+}
